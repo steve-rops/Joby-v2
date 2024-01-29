04bcd691a89a8787bb67f7827f2fef73185cc1a1
steve-rops <somebiss@gmail.com>
2024-01-30 00:58:45 +0200

    added the avatars on the app navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { useUserContext } from "../contexts/UserContext";
+import Avvvatars from "avvvatars-react";
 
 export function Navbar() {
   const { user } = useUserContext();
@@ -7,12 +8,8 @@ export function Navbar() {
       <h1 className="text-3xl text-indigo-800">Joby</h1>
       <div className="flex gap-2 items-center bg-indigo-500 text-white rounded-md p-2">
         <span className="text-xl">👋🏻</span>
-        <span>Hello, {user} </span>
-        <img
-          src="https://api.dicebear.com/7.x/adventurer/svg?scale=200"
-          alt="avatar"
-          className="h-[35px] rounded-xl "
-        ></img>
+        <span>Hi {user}</span>
+        <Avvvatars value={`${user}`} />
       </div>
     </div>
   );
