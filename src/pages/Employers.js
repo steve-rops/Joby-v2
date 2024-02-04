73a1a0c2f04d52b106578a07c69894d6f6839f47
steve-rops <somebiss@gmail.com>
2024-02-04 22:43:10 +0200

    new changes

--- src/pages/Employers.js
+++ src/pages/Employers.js
@@ -4,7 +4,20 @@ function Employers() {
   return (
     <div>
       <NavBar />
-      <span>Employers</span>
+      <div className="p-2 px-8">
+        <span className="text-lg">Active Employers: </span>
+        <div className="w-fit h-20 bg-indigo-100 border-2 border-indigo-700 rounded-md shadow-md grid place-items-center">
+          <span className="px-2">
+            <a
+              href="https://developer.adzuna.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Adzuna API
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
